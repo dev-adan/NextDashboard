0460f36a5b73736d8f1067c52894d8a75cf4baab
Adan <adanaligakhar@gmail.com>
2024-04-30 19:08:17 +0500

    charts done

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Card, AreaChart, Title, Text } from "@tremor/react";
 
 const data = [
   {
@@ -19,5 +20,21 @@ const data = [
 ];
 
 export default function Chart() {
-  return <div>chart</div>;
+  return (
+    <Card className="mt-8">
+      <Title>Performance</Title>
+      <Text>Comparison between Sales and Profit</Text>
+      <AreaChart
+        className="mt-4 h-80"
+        data={data}
+        categories={["Sales", "Profit"]}
+        index="Month"
+        colors={["indigo", "fuchsia"]}
+        valueFormatter={(number: number) =>
+          `$ ${Intl.NumberFormat("us").format(number).toString()}`
+        }
+        yAxisWidth={60}
+      />
+    </Card>
+  )
 }
